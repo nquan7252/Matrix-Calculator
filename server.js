da4869139397199ee725678ef9be453eeef331ad
Quan <nquan7252@gmail.com>
2022-05-31 19:39:07 -0700

    new

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express=require('express')
 var path=require('path')
 var app=express();
-const PORT=3000||process.env.PORT;  
+const PORT=process.env.PORT||3000;  
 app.use(express.static(path.join(__dirname,'build')))
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'build','index.html'))
